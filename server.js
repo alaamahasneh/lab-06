b13a3e0b1e32130155b9ae4e00b7168759465fd4
mohammad alsboul <mohammad.alspoole@gmail.com>
2019-12-22 21:32:02 +0200

    finish lab-06

--- server.js
+++ server.js
@@ -31,8 +31,8 @@ Weather.all = []
 server.get('/weather', (request, response) => {
     const weatherData = require('./data/darksky.json');
     weatherData.daily.data.forEach( data => {
+        let date = new Date(data.time * 1000).toDateString();
         let sum = data.summary;
-       let date = new Date(data.time * 1000);
        new Weather(sum,date);
 
     });
